f4794c6349169a588244cd5d58bb8449ef618e8c
R.Vile <rodolfoaugusto@outlook.com>
2016-05-31 01:57:34 -0300

    Update signUsers.c
    update, with search user by: name, and remove than.

--- signUsers.c
+++ signUsers.c
@@ -16,6 +16,39 @@ struct Usuario {
   int flag;
 };
 
+// busca por nome do usuario;
+int busca(char *arquivom char nome[]) {
+  struct Usuario user;
+  int i = 0;
+
+  FILE *arq;
+  if (!(arq = fopen(arquivo, "rb"))) {
+    printf("error ao ler arquivo\n");
+    return;
+  }
+  fseek(arq, 0, SEEK_SET);
+  fread(&user, sizeof(struct Usuario), 1, arq);
+  while (!feof(arq)) {
+    if (user.flag == 1 && (((strcmp(user.nome, nome) == 0) || (strcmp(user.login, nome) == 0)))) {
+      fclose(arq);
+      return i;
+    }
+    else
+      i++;
+
+    fread(&user, sizeof(struct Usuario), 1, arq);
+  }
+
+  fclose(arq);
+
+  return -1;
+
+} /*end function*/
+
+
+
+}
+
 void inserirUsuario (char *arquivo) {
   system("cls");
   struct Usuario user;
@@ -64,5 +97,36 @@ void inserirUsuario (char *arquivo) {
   if (fclose(arq)) {
     printf("Erro ao tentar fechar arquivo\n");
   }
-  
-}/*end void void*/
+
+}/*end void*/
+
+void removerUsuario (char *arquivo, char nome[]) {
+  struct Usuario user;
+  int chave = busca(arquivo, nome);
+
+  FILE *arq;
+
+  if (!(arq = fopen(arquivo, "r+b"))) { /*abre o arquivo tanto para ler como escrever em modo binario*/
+    printf("Error ao abrir arquivo\n");
+    return;
+  }
+
+  if (chave == -1) {
+    printf("Usario nao exite\n");
+  } else {
+    fseek (arq, chave * sizeof(struct Usuario), SEEK_SET);
+    fread(&user,sizeof(struct Usuario),1, arq);
+    printf("O Usuario de login: %s será deletado \n", nome);
+    user.flag = 0;
+
+    fseek(arq, chave * sizeof(struct Usuario), SEEK_SET);
+    if (fwrite(&user, sizeof(struct Usuario), 1, arq) != 1) {
+			printf("Erro ao deletar. \n");
+    } else {
+      printf("Usuario deletado com sucesso\n");
+    }
+
+    if (fclose(arq)) {
+      printf("Error ao tentar fechar arquivo\n");
+    }
+  } /*end void*/
